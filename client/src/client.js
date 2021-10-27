87544de62f8b80d22cfa18472d89767ad443b3b9
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-28 00:17:03 +0100

    vars

--- client/src/client.js
+++ client/src/client.js
@@ -5,8 +5,8 @@ let accessToken;
 
 
 if(process.env.NODE_ENV === 'production'){
-    spaceId = process.env.SPACE_ID
-    accessToken = process.env.ACCESS_TOKEN
+    spaceId = process.env.REACT_APP_SPACE_IDS
+    accessToken = process.env.RAECT_APP_ACCESS_TOKENS
 }else{
     spaceId = process.env.REACT_APP_SPACE_ID
     accessToken = process.env.REACT_APP_ACCESS_TOKEN
