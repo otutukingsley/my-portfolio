e3d3a4bf212a5dfb506c2ec155d6b4066a6bf374
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-27 01:26:54 +0100

    fixed structure

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 require('dotenv').config()
 
 //middleware
@@ -16,3 +17,9 @@ app.listen(PORT, () => {
 app.use("/send", require("./routes/send"));
 
 //Serve Static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('../build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve()))
+}
