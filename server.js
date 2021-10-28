6aed61041adfc318ecd8286ee4f0eb0396659bf8
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-28 21:07:06 +0100

    fixing

--- server.js
+++ server.js
@@ -23,8 +23,11 @@ if (process.env.NODE_ENV == 'production') {
   )
 }
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 const HOST = process.env.HOST || '0.0.0.0'
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on port: ${PORT}, host: ${HOST}`)
+
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'), HOST, () => {
+  console.log(`Server is running on port 5000, host: ${HOST}`)
 })
