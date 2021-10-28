c406bc4d9ce9ecd1a61205f0eaf8d5a91cada14d
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-28 10:25:25 +0100

    fixing variables

--- client/src/client.js
+++ client/src/client.js
@@ -1,19 +1,6 @@
 import * as contentful from 'contentful';
 
-
-let spaceId;
-let accessToken;
-
-
-if(process.env.NODE_ENV === 'production'){
-    spaceId = process.env.REACT_APP_SPACE_ID
-    accessToken = process.env.RAECT_APP_ACCESS_TOKEN
-}else{
-    spaceId = process.env.REACT_APP_SPACE_ID
-    accessToken = process.env.REACT_APP_ACCESS_TOKEN
-}
-
 export const client = contentful.createClient({
-    space: spaceId,
-    accessToken: accessToken,
+    space: process.env.REACT_APP_SPACE_ID,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 })
