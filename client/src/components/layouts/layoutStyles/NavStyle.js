96ae127864850d8bc86eb832e8e3fb58d90d6a47
otutukingsley <chineduotutu3@gmail.com>
2022-03-06 15:00:42 +0100

    fixed navigation link styled

--- client/src/components/layouts/layoutStyles/NavStyle.js
+++ client/src/components/layouts/layoutStyles/NavStyle.js
@@ -1,89 +1,70 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const Nav = styled.nav`
-position: sticky;
-top: 0;
-left: 0;
-right:0;
-z-index: 10;
-box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
-background-color: ${({ theme }) => theme.colors.black};
-color: ${({ theme }) => theme.colors.navColor};
-
-@media (max-width: 1070px) {
-border-bottom: 1px solid ${({ theme }) => theme.colors.darkFont};
-box-shadow: unset;
-}
-
-.nav-wrapper{
- max-width: 75.25rem;
- margin: 0 auto;
- display: flex;
- align-items: center;
- justify-content: space-between;
- padding: 1rem 2.2rem;
-
-
-
- @media(max-width: 290px) {
-  width: 100%;
-  padding: 1rem;
- }
-
-
- .hamburger-btn{
-   background: transparent !important;
-   border: none !important;
-   cursor: pointer !important;
-   color: ${({ theme }) => theme.colors.skyBlue};
-
-   &:hover{
-    background: transparent !important;
-   }
- }
-
- .nav-heading{
-  color: ${({ theme }) => theme.colors.white};
-
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  background-color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.navColor};
 
   @media (max-width: 1070px) {
-    font-size: 1.3rem;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.darkFont};
+    box-shadow: unset;
   }
-}
 
- .navigation{
-   display: flex;
-   align-items: center;
-   justify-content: stretch;
-   gap: 1.5rem;
+  .nav-wrapper {
+    max-width: 75.25rem;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 2.2rem;
 
-   li {
-    position: relative;
+    @media (max-width: 290px) {
+      width: 100%;
+      padding: 1rem;
+    }
 
-    a{
-      transition: color .5s ease-in-out;
-      &:hover{
-        color:${({ theme }) => theme.colors.white};
-        border-bottom: 4px solid ${({ theme }) => theme.colors.skyBlue};
+    .hamburger-btn {
+      background: transparent !important;
+      border: none !important;
+      cursor: pointer !important;
+      color: ${({ theme }) => theme.colors.skyBlue};
+
+      &:hover {
+        background: transparent !important;
       }
     }
 
-    &:after {
-        content: '';
-        display: block;
-        padding-top: 0.188rem;
-        margin: auto;
-        height: .05rem;
-        width: 0px;
-        background: transparent;
-        transition: width .5s ease, background-color .5s ease;
-      }
+    .nav-heading {
+      color: ${({ theme }) => theme.colors.white};
 
-      &:hover:after {
-        width: 100%;
-        background:${({ theme }) => theme.colors.skyBlue} ;
+      @media (max-width: 1070px) {
+        font-size: 1.3rem;
+      }
+    }
+
+    .navigation {
+      display: flex;
+      align-items: center;
+      justify-content: stretch;
+      gap: 1.5rem;
+
+      li {
+        position: relative;
+        a {
+          transition: color 0.5s ease-in-out;
+          display: block;
+          width: 100%;
+          &:hover {
+            color: ${({ theme }) => theme.colors.white};
+            border-bottom: 4px solid ${({ theme }) => theme.colors.skyBlue};
+          }
+        }
       }
     }
   }
-}
 `
