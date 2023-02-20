a737dcf064230571d152575a156098d5f265149e
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2023-02-20 09:55:53 +0100

    added links to home

--- client/src/components/pages/home/IntroStyle.js
+++ client/src/components/pages/home/IntroStyle.js
@@ -34,4 +34,27 @@ margin: 0 auto;
 padding: 2rem;
 width: 100%;
 height: 100%;
+
+.home-socials {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.6rem;
+  margin-top: 15px;
+
+  .github,
+  .linkedin {
+    padding: 0;
+  }
+
+  .social-icon {
+    color: ${({ theme }) => theme.colors.darkFont};
+    font-size: 25px;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.skyBlue};
+    }
+  }
+}
 `
--- client/src/components/pages/home/Myintro.js
+++ client/src/components/pages/home/Myintro.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container, Intro } from "./IntroStyle";
 import Resume from "../../../documents/ChineduOtutuCV.pdf";
+import {
+  AiFillTwitterSquare,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
 
 const Myintro = () => {
   return (
@@ -9,6 +14,37 @@ const Myintro = () => {
         <div className="intro-desc">
           <h1 className="intro-title">Otutu Kingsley</h1>
           <h3 className="sub-title">Software Engineer</h3>
+
+          <div className="home-socials">
+            <span className="github">
+              <a
+                href="https://github.com/otutukingsley"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiOutlineGithub className="social-icon" />
+              </a>
+            </span>
+            <span className="linkedin">
+              <a
+                href="https://www.linkedin.com/in/otutu-chinedu-2a5b30195/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <AiOutlineLinkedin className="social-icon" />
+              </a>
+            </span>
+            <span className="linkedin">
+              <a
+                href="https://twitter.com/tutuMantutu"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <AiFillTwitterSquare className="social-icon" />
+              </a>
+            </span>
+          </div>
+
           <a
             href={Resume}
             target="_blank"
