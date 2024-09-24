abe0be251c37e594e4cc204d5b71a86e21bfcc56
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2024-09-24 16:12:23 +0100

    fake age

--- client/src/components/pages/about/About.js
+++ client/src/components/pages/about/About.js
@@ -53,7 +53,7 @@ const About = () => {
               <ul className="infos">
                 <li>
                   <span className="title">Age</span>{" "}
-                  <span className="value">{age(1996)}</span>
+                  <span className="value">{age(1995)}</span>
                 </li>
                 <li>
                   <span className="title">Residence</span>{" "}
